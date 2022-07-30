43693c3c139fe2dcfd3ceccc7111e82bd797e0e3
Trigg3r-Happ8 <106489429+Trigg3r-Happ8@users.noreply.github.com>
2022-07-30 18:51:22 +0200

    Update en.js

--- languages/en.js
+++ languages/en.js
@@ -57,10 +57,10 @@ module.exports = {
             description: 'With GiveAways, you can improve your social media accounts and get organic increases by creating advanced sweepstakes!',
             buttonText: 'Add to Discord',
             items: [
-                 { icon: 'fal fa-star', title: 'Easily Create Conditional Giveaway', description: 'You can have the members of your dreams in a short time. You can get organic increase by opening conditional giveaways by following your social media accounts such as YouTube, Twitch, TikTok, etc. on GiveAways. Why not try opening a raffle for more information?' },
+                 { icon: 'fal fa-star', title: 'Easily Create Conditional Giveaways', description: 'You can have the members of your dreams in a short time. You can get organic increase by opening conditional giveaways by following your social media accounts such as YouTube, Twitch, TikTok, etc. on GiveAways. Why not try opening a raffle for more information?' },
                  { icon: 'fal fa-star', title: 'Build Your Own Giveaways', description: 'You can quickly add the bot to the server and create and customize your own giveaway on the site. You can check who has participated in the giveaway in detail via the Dashboard.'},
                  { icon: 'fal fa-star', title: 'Discover Giveaways ', description: 'You can participate in the giveaway according to you in the Discover section and win prizes, and you can see the remaining time and detailed information on the panel.' },
-                 { icon: 'fal fa-star', title: 'Self Feel Special', description: 'By purchasing Boost, you can giveaway more than 20 simultaneous giveaways on your server, however, you can repeat and embed 20 times on a giveaway. You can make it safe by adding a password to your giveaways.' }
+                 { icon: 'fal fa-star', title: 'Feel Special Yourself', description: 'By purchasing Boosts, you can giveaway more than 20 simultaneous gifts on your server! However, you can only repeat and embed 20 times on a giveaway. You can make it safe by adding a password to your giveaways.' }
             ]
         },
         advanced: [
